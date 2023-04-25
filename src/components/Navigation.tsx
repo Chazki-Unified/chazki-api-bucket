187557785e8b693b463896b91d0fbdc8d003ead6
elazon <elazon01@gmail.com>
2023-04-25 11:02:16 -0500

    Remove deprecated APIs from NavBar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,9 +7,9 @@ export const Navigation = () => {
   return (
     <nav className="topnav">
       <NavLink to="/tonny"><img src={"https://raw.githubusercontent.com/Chazki/chazki-api-bucket/main/src/resources/logo.png"} style={{width:"95px"}}></img></NavLink>
-      <NavLink to="/maximo">Maximo API</NavLink>
-      <NavLink to="/aventon">Aventon API</NavLink>
-      <NavLink to="/chile">Chile Legacy API</NavLink>
+      {/*<NavLink to="/maximo">Maximo API</NavLink>*/}
+      {/*<NavLink to="/aventon">Aventon API</NavLink>*/}
+      {/*<NavLink to="/chile">Chile Legacy API</NavLink>*/}
       <NavLink to="/fifo">Chile Fifo API</NavLink>
       <NavLink to="/tonny">Tonny API</NavLink>
       <NavLink to="/peru">Perú Legacy API</NavLink>
